ba186b6e7f665fb48d311abc5544df2ae33a9e7d
Rafael Portela <rafaelportela90@gmail.com>
2015-02-01 18:39:18 +0200

    Espresso 2.0 up and running.

---
+++ app/src/androidTest/java/br/com/rafaelportela/espresso/MainActivityTest.java
@@ -0,0 +1,27 @@
+package br.com.rafaelportela.espresso;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testMainActivity() throws Exception {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+}
